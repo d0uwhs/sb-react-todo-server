7ec0e4e0ceaa03587799d917e7b2b6c5655740f0
d0uwhs <d0uwhs@gmail.com>
2022-10-25 14:18:24 +0900

    Initial commit & Add .gitignore

---
+++ src/main/java/com/d0uwhs/sbreacttodoserver/SbReactTodoServerApplication.java
@@ -0,0 +1,13 @@
+package com.d0uwhs.sbreacttodoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbReactTodoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbReactTodoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/d0uwhs/sbreacttodoserver/SbReactTodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d0uwhs.sbreacttodoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbReactTodoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
